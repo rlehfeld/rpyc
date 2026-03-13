4aae85a44d43cab8d7786b714326c50758043210
René Lehfeld <54720674+rlehfeld@users.noreply.github.com>
2026-03-13 19:22:52 +0100

    fix copy&paste error

--- rpyc/utils/server.py
+++ rpyc/utils/server.py
@@ -74,7 +74,7 @@ class Server(object):
                 raise ValueError("socket_path is mutually exclusive with: hostname, port, ipv6")
             self.listener = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
             if nodelay:
-                s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                self.listener.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             self.listener.bind(socket_path)
             # set the self.port to the path as it's used for the registry and logging
             self.host, self.port = "", socket_path
@@ -93,7 +93,7 @@ class Server(object):
                 # "unexpected behavior" (quoting MSDN)
                 self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             if nodelay:
-                s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                self.listener.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             self.listener.bind(address)
             self.listener.settimeout(listener_timeout)
 
